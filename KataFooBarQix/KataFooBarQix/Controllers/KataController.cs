077a11f7484f35d12f5a971f1f2e20d7635c9182
eric-longueville <118602139+eric-longueville@users.noreply.github.com>
2023-01-20 10:20:19 +0100

    Version avec Dictionary

---
+++ KataFooBarQix/KataFooBarQix/Controllers/KataController.cs
@@ -0,0 +1,21 @@
+﻿using KataFooBarQix.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KataFooBarQix.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KataController : ControllerBase
+    {
+        private readonly IComputer _computer = new KataComputer();
+        public KataController(IComputer newComputer)
+        {
+            _computer = newComputer;
+        }
+        [HttpGet(Name="Compute")]
+        public string Index(string rawInput)
+        {
+            return _computer.Compute(rawInput);
+        }
+    }
+}
---
+++ KataFooBarQix/KataFooBarQix/Services/KataComputer.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace KataFooBarQix.Services
+{
+    public interface IComputer
+    {
+        string Compute(string input);
+    }
+    public class KataComputer : IComputer
+    {
+        public string Compute(string input)
+        {
+            StringBuilder result = new();
+            Dictionary<string, string> mapping = new();
+            mapping.Add("3", "Foo");
+            mapping.Add("5", "Bar");
+            mapping.Add("7", "Qix");
+            mapping.Add("0", "*");
+            try
+            {
+                foreach(var currentCouple in mapping.Where(x => x.Key != "0"))
+                {
+                    if(int.Parse(input) % int.Parse(currentCouple.Key) == 0)
+                    {
+                        result.Append(currentCouple.Value);
+                    }
+                }
+                foreach (char character in input)
+                {
+                    if(mapping.Any(x => x.Key == character.ToString()))
+                    {
+                        result.Append(mapping.First(x => x.Key == character.ToString()).Value);
+                    }
+                }
+                if(String.IsNullOrEmpty(result.ToString()))
+                {
+                    result.Append(input.Replace('0', '*'));
+                }
+            }
+            catch(Exception e)
+            {
+                result = new();
+                result.Append("Error " + e.StackTrace + " : " + e.Message);
+            }
+            return result.ToString();
+        }
+    }
+}
